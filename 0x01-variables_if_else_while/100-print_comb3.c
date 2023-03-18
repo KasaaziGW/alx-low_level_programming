09f7ceebf7e7e8d40b2c497d1c579df5057bdd8e
George William Kasaazi <gkasaazi@andrew.cmu.edu>
2023-03-18 03:07:43 +0200

    ALX Variables If-else While

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,16 +20,16 @@ int main(void)
 			if (e != i && e < i)
 			{
 				putchar(e);
-		         	putchar(i);
-			 	if (i == 57 && e == 56)
-			 	{
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
 					break;
-			 	}
-			 	putchar(',');
-			 	putchar(' ');
-		 	}	
-		 	i++;
-	 	}	
+				}
+				putchar(',');
+				putchar(' ');
+			}	
+			i++;
+		}	
 		e++;
 	}
 	putchar('\n');
